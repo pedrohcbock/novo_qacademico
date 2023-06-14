4bae26a5bbb5462d569efe8f7c0e85c294d79818
Pedro Henrique Bock <pedrohenriquecbock@gmail.com>
2023-06-14 11:49:56 -0300

    falta terminar a function de registrar, a rota e o html

--- app/Http/Controllers/AlunosController.php
+++ app/Http/Controllers/AlunosController.php
@@ -1,10 +1,20 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Aluno;
 use Illuminate\Http\Request;
 
 class AlunosController extends Controller
 {
-    //
+    public function addSave(Request $form)
+    {
+        //dd($form); -> Vardump e die
+        $dados = $form->validate([
+            'name' => 'required|unique:produtos|min:3',
+            'price' => 'required|numeric|min:0',
+            'quantity' => 'required|integer|min:0',
+        ]);
+        Aluno::create($dados);
+        return redirect()->route('produtos');
+    }
 }
