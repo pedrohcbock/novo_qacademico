6e33b2ed4d42ae3c45828721bce7baf4214ad9d0
Phiszy <pedrohenriquecbock@gmail.com>
2023-07-03 14:17:50 -0300

    arrumei as migrations

--- database/migrations/2023_07_03_171640_create_boletim_table.php
+++ database/migrations/2023_07_03_171640_create_boletim_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('boletim', function (Blueprint $table) {
             $table->id();
             $table->foreignId('idMateria')->constrained('materias');
-            $table->foreignId('idNota')->constrained('nota');
+            $table->foreignId('idNota')->constrained('notas');
         });
     }
 
