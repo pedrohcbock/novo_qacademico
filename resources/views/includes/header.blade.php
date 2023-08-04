9a5bc9970306d09d67cd1e841fb7973611407b55
Marcelo Staudt <marcelo.121205@bento.local>
2023-08-04 16:59:08 -0300

    Feat: tela de menu

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../style/header.css">
+    <script src="https://kit.fontawesome.com/0e91a3ee41.js" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="itens">
+            <h1><div class="q">Q-ac</div>adêmico</h1>
+            <nav>
+
+            </nav>
+        </div>
+    </header>
+
+    <section>
+        <div class="form-box">
+           
+        </div>
+    </section>
+</body>
+</html>
