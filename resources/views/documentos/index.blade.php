26c66e324a092c6b93e67904cc55582d75878c5d
unknown <m.henriquestaudt@gmail.com>
2023-08-23 23:19:16 -0300

    solicitar documentos arrumado

--- resources/views/documentos/index.blade.php
+++ resources/views/documentos/index.blade.php
@@ -2,62 +2,73 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/remixicon@3.2.0/fonts/remixicon.css" rel="stylesheet">
-    <link rel="stylesheet" href="../style/novodoc.css">
-    <script src="../js/solicitar.js" defer></script>
-
-    <title>Solicitação de Documento</title>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Sidebar</title>
+    <link rel="stylesheet" href="../style/barra.css" />
 </head>
 
 <body>
+    @extends('includes.header-aluno')
+    <div class="sidebar">
+        <ul>
+            <span>Navegar</span>
+            <li>
+                <a href="{{ route('includes.header-aluno') }}"><ion-icon name="home-outline"></ion-icon>
+                    <p>Menu</p>
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('notas.index') }}"><ion-icon name="stats-chart-outline"></ion-icon>
+                    <p>Notas</p>
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('calendarios.index') }}"><ion-icon name="today-outline"></ion-icon>
+                    <p>Calendário</p>
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('documentos.index') }}"><ion-icon name="folder-open-outline"></ion-icon>
+                    <p>Documentos</p>
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('refeitorios.index') }}"><ion-icon name="today-outline"></ion-icon>
+                    <p>Refeitório</p>
+                </a>
+            </li>
+        </ul>
+        <ul>
+            <li class="switch-theme">
+            </li>
+            <li>
+                <a href="{{ route('logout') }}"><ion-icon name="log-out-outline"></ion-icon>
+                    <p>Deslogar</p>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 
     <section>
-        <div class="sub_titulo">
-            <h1 class="sub">Solicitar Documentos</h1>
-            <h2 class="sub_sub">Documentos Solicitados</h2>
-        </div>
-        <div class="logoQuest">
-            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
-                class="bi bi-journal-text" viewBox="0 0 16 16">
-            </svg>
-        </div>
-        <div class="container">
-            <button class="abre_modal" id="openModal">
-                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"
-                    class="bi bi-folder-plus" viewBox="0 0 16 16">
-                </svg>
-                <p>Nova Solicitação</p>
-            </button>
-        </div>
-        <div class="meusQuest">
-
-
-            @foreach ($documentos as $documento)
-                <h4 class="nomeQuest">{{ $documento->tipo }}
-                    <a href="{{ route('documentos.download', ['id' => $documento->id]) }}" class="btn-dow">Download</a>
-                </h4>
-            @endforeach
-        </div>
-
-        <div class="modal-overlay" id="modalOverlay">
-            <div class="modal">
-                <form>
-                    <label for="filtroTipoModal" class="filtro-tipo">Selecione o tipo de documento que você
-                        deseja:</label>
-                    <select id="filtroTipoModal" class="seleciona" name="filtro_tipo">
-                        <option value="Atestado de Matricula">Atestado de Matricula</option>
-                        <option value="Boletim">Boletim</option>
-                        <option value="Histórico Escolar">Histórico Escolar</option>
-                    </select>
-                    <br>
-                    <button class="enviar_solicita" type="submit">Enviar Solicitação</button>
-                </form>
-            </div>
+        <div class="form-box">
+            <main>
+                <div class="cima" style="margin-left: 13.5vw">
+                    <h1>Solicitação de Documentos</h1>
+                </div>
+                <div class="meusQuest">
+                    @foreach ($documentos as $documento)
+                    <a href="{{ route('documentos.download', ['id' => $documento->id]) }}" class="btn-dow">
+                        <button class="nomeQuest">{{ $documento->tipo }}</button>
+                    </a>
+                    @endforeach
+                </div>
+            </main>
         </div>
     </section>
-
 </body>
 
 </html>
